02073522d2cb63488d63912a1592205fe497f28f
Stefano Degiorgi <stef.degiorgi@bluewin.ch>
2023-08-07 14:06:24 +0200

    changing connection

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "login_db";
+$dbhost = "sql313.infinityfree.com";
+$dbuser = "if0_34765021";
+$dbpass = "FXNbfXQgFieqen";
+$dbname = "if0_34765021_login_db";
 
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
 {
